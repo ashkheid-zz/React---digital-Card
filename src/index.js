a9c26ca6927d355c0b4becf1c8ece6595d52fee7
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2022-02-01 18:16:50 +0330

    First try

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//#region Creating root element inside the <body> tag
+//Creating <div> element
+const rootElement = document.createElement('div');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//assigning #root id to it
+rootElement.setAttribute('id', 'root');
+
+//appending to <body>
+document.body.append(rootElement);
+//#endregion
+
+ReactDOM.render(<h1>Hello from React 👋🏻</h1>, rootElement);
