46156369dc9a70f51e11f8bc30f0365d0537704b
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2022-02-01 22:07:11 +0330

    Create Interests.js

---
+++ src/components/Interests.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Interests() {
+  return (
+    <div className="card__interests">
+      <h2 className="card__interests--title">Interests</h2>
+      <p className="card__interests--content">
+        My interests are playing & listening to music, drawing, board games,
+        solving puzzles, learning & creating new things, and reading books &
+        articles about Programming, Psychology, Psymentology, Art, and Design.
+      </p>
+    </div>
+  );
+}
