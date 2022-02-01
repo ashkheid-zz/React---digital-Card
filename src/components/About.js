d6647eb03a8801f362d974927bf1610da1dc2c51
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2022-02-01 22:07:00 +0330

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="card__about">
+      <h2 className="card__about--title">About</h2>
+      <div className="card__about--content">
+        <p>
+          I am a Junior Front-end Developer with the 7 years of experience in Graphic Design and 2 years of experience in UI/UX Design; from Tehran, IRAN.
+        </p>
+        <p>
+          I can also build high-performance Excel worksheets, create impressive
+          PowerPoint presentations & transform your plain narration into an
+          ear-satisfying podcast.
+        </p>
+      </div>
+    </div>
+  );
+}
